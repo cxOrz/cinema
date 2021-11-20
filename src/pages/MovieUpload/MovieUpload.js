d9745a057fcc7284fc95e3df1631ae3e0501faed
miaochenxi <miaochenxi@outlook.com>
2021-11-21 02:14:22 +0800

    电影上传完成

--- src/pages/MovieUpload/MovieUpload.js
+++ src/pages/MovieUpload/MovieUpload.js
@@ -1,10 +1,35 @@
-import { Typography } from '@material-ui/core'
-import React from 'react'
+import Button from '@material-ui/core/Button'
+import axios from 'axios'
+import React, { useRef } from 'react'
+import { UPLOAD } from '../../configs/api'
 
 function MovieUpload() {
+  const fileInput = useRef(null)
+
+  function upload() {
+    const formData = new FormData()
+    formData.append('file', fileInput.current.files[0])
+    axios({
+      method: 'POST',
+      url: UPLOAD.URL,
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      },
+      data: formData
+    }).then((res) => {
+      console.log(res.data)
+    })
+  }
+
   return (
     <div>
-      <Typography variant="h1">影片上传</Typography>
+      <input ref={fileInput}
+        type="file"
+        accept="video/*"
+        style={{ display: 'none' }}
+        onChange={upload}
+      />
+      <Button onClick={() => { fileInput.current.click() }} variant="contained">上传</Button>
     </div>
   )
 }
